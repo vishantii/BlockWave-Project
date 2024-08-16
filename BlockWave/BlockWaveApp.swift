e4a56a0952b46f16742abeb35767611f5e4dc014
Farhan <mohamad.farhan@aleph-labs.com>
2024-08-16 14:09:55 +0700

    Initial Commit

---
+++ BlockWave/BlockWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockWaveApp.swift
+//  BlockWave
+//
+//  Created by Mohamad Farhan on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BlockWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockWave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlockWave
+//
+//  Created by Mohamad Farhan on 16/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
